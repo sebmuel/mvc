29e76b6f956e584f21adbf350db5e706221e9e4f
Whusthat <mueller.sebastian117@gmail.com>
2022-05-01 14:18:20 +0200

    Init commit

---
+++ core/Router.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+
+    public Request $request;
+    protected array $routes = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path =  $this->request->getPath();
+        $method = $this->request->getMethod();
+        echo '<pre>';
+        var_dump($this->routes);
+        echo '</pre>';
+        $callback = $this->routes[$method][$path];
+
+        var_dump($callback);
+    }
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once __DIR__ . '/vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+
+$app->router->get('/', function () {
+    return "Home";
+});
+
+$app->router->get('/contact', function () {
+    return "Contact";
+});
+
+
+$app->run();
